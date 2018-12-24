6686a24f62d1fc72fd00eb15f9cf84da346e2a99
Alexander Stankevich <a_stankevich@twistellar.com>
2018-12-24 13:41:32 +0300

    updated labs & deleted repeating labs

--- triangle/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(isAvaliable(1,1,1));
-    }
-    public static boolean isAvaliable(double a, double b, double c) {
-            return !(a <= 0) && !(b <= 0) && !(c <= 0) && a + b > c && b + c > a && a + c > b;
-    }
-}
